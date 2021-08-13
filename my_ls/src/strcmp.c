a54c78058a025827cb8f8e46794f79e3910e24a3
Abdullah Dhneem <67286684+DHNEEMAF@users.noreply.github.com>
2021-08-13 04:18:03 -0700

    Create strcmp.c

---
+++ my_ls/src/strcmp.c
@@ -0,0 +1,11 @@
+#include "my_ls.h"
+
+int my_strcmp(char* param_1, char* param_2)
+{
+    int i = 0;
+
+    while (param_1[i] != '\0' && param_1[i] == param_2[i])
+        i++;
+
+    return param_1[i] - param_2[i];
+}
